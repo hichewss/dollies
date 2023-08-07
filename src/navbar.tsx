49f587e5ae93959c6858925d158ab0fd47afac71
Chuyi Zhang <hichews@stanford.edu>
2023-08-06 22:39:59 -0700

    Added navbar content

--- src/navbar.tsx
+++ src/navbar.tsx
@@ -3,8 +3,12 @@ import { CloseOutIcon, DollieBoot } from "./images";
 
 function Navbar() {
   return (
-    <div className="navbar">
+    <div className="navbar vertical-box">
       <div className="close-out">{CloseOutIcon()}</div>
+      <div className="navbar-options">HOME</div>
+      <div className="navbar-options">ARCHIVE</div>
+      <div className="navbar-options">AUDITION</div>
+      <div className="navbar-options">CONTACT</div>
     </div>
   );
 }
