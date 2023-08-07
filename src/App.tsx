dd2f250c09aca0d0ee13c68a39281aa32372f527
Chuyi Zhang <hichews@stanford.edu>
2023-08-06 19:47:16 -0700

    Added fonts and colors

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import React from "react";
 
 function App() {
-  return <div>It's time to get funky!</div>;
+  return <h1>It's time to get funky!</h1>;
 }
 
 export default App;
