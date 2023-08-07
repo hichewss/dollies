ef5ec332e83b880f901aab94de62c9ee43f91e4b
Chuyi Zhang <hichews@stanford.edu>
2023-08-07 00:16:11 -0700

    Added background images

---
+++ src/components/images.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export function CloseOutIcon() {
+  return (
+    <svg
+      width="28"
+      height="27"
+      viewBox="0 0 28 27"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <rect
+        width="32.5796"
+        height="5"
+        rx="2.5"
+        transform="matrix(0.710217 -0.703983 0.710217 0.703983 1 23.155)"
+        fill="#FFFBF7"
+      />
+      <rect
+        width="32.5796"
+        height="5"
+        rx="2.5"
+        transform="matrix(-0.710217 -0.703983 0.710217 -0.703983 23.1387 26.4553)"
+        fill="#FFFBF7"
+      />
+    </svg>
+  );
+}
--- src/navbar.tsx
+++ src/navbar.tsx
@@ -1,9 +1,29 @@
 import React from "react";
-import { CloseOutIcon, DollieBoot } from "./images";
+import { CloseOutIcon } from "./components/images";
+import BootOne from "./images/boot_one.png";
+import BootTwo from "./images/boot_two.png";
+import BootThree from "./images/boot_three.png";
+import BootFour from "./images/boot_four.png";
 
 function Navbar() {
   return (
     <div className="navbar vertical-box">
+      <img
+        className="boots-background about-logos top boot-short-left"
+        src={BootOne}
+      />
+      <img
+        className="boots-background about-logos top boot-far-left"
+        src={BootTwo}
+      />
+      <img
+        className="boots-background about-logos bottom boot-left"
+        src={BootThree}
+      />
+      <img
+        className="boots-background about-logos bottom boot-right"
+        src={BootFour}
+      />
       <div className="close-out">{CloseOutIcon()}</div>
       <div className="navbar-options">HOME</div>
       <div className="navbar-options">ARCHIVE</div>
