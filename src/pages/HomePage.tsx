938a98b4f12bce05fe2263552041cdcaf56c2207
Chuyi Zhang <hichews@stanford.edu>
2023-12-28 12:05:18 -0800

    right align menu

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import Dollies from "../images/home/dollies.png";
 import { DolliesNeverDie } from "./ArchivePage";
+import { RightArrow } from "../components/images";
 
 function HomePage() {
   return (
     <div className="vertical-box">
-      <div>
-        <img
-          src={Dollies}
-          width="100%"
-          height="100%"
-          alt="Stanford Dollies 23-24"
-        />
+      <div className="end-box small-right-margin">
+        <p className="color-white absolute">MENU</p>
+      </div>
+      <img
+        src={Dollies}
+        width="100%"
+        height="100%"
+        alt="Stanford Dollies 23-24"
+      />
+      <div className="absolute home-header-box center-box">
+        <div className="home-header color-white">THE STANFORD DOLLIES</div>
       </div>
       <DolliesNeverDie />
     </div>
