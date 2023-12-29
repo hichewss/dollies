08c13052f151e84ccc8ebc1cbb5b6a6a8330d541
Chuyi Zhang <hichews@stanford.edu>
2023-12-28 16:47:28 -0800

    more drafting to home page

---
+++ src/components/BlackFooter.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function BlackFooter() {
+  return (
+    <div className="black-footer">
+      <p className="color-white">Home</p>
+    </div>
+  );
+}
+
+export default BlackFooter;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Dollies from "../images/home/dollies.png";
+import Sash from "../images/home/sash.png";
 import { DolliesNeverDie } from "./ArchivePage";
-import { RightArrow } from "../components/images";
+import BlackFooter from "../components/BlackFooter";
 
 function HomePage() {
   return (
@@ -18,7 +19,34 @@ function HomePage() {
       <div className="absolute home-header-box center-box">
         <div className="home-header color-white">THE STANFORD DOLLIES</div>
       </div>
+      <div className="home-text-box">
+        <div className="home-text-margin">
+          <h1 className="color-red">
+            SINCE 1953, DOLLIES HAVE APPEARED AT SPORTING EVENTS AND RALLIES
+            WITH THE STANFORD BAND AND TREE
+          </h1>
+          <div className="small-bottom-margin" />
+          <div className="home-text-margin">
+            <p className="color-red">
+              Dollies dance entirely of their own choreography and past Dollies’
+              design. Their style ranges from modern to ballet to jazz, but is
+              always unique and impressive.
+            </p>
+            <div className="small-bottom-margin" />
+            <p className="color-red">
+              They can always be recognized, not only by their stunning attire,
+              but also by their charming smiles and throng of adoring fans.
+              Dollies are often involved in various dance ensembles on campus
+              and off as members of and choreographers for many programs.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="white-triangle" />
+      </div>
       <DolliesNeverDie />
+      <BlackFooter />
     </div>
   );
 }
