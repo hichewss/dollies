ff5df8e927a39b3856ab277add79bbca79c5ade9
Chuyi Zhang <hichews@stanford.edu>
2024-01-04 21:03:49 -0800

    updated years with dropdowns

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
+import { increments } from "../data/archive";
 
 function NavBar() {
   const YEARS = [
@@ -12,15 +13,35 @@ function NavBar() {
     "2020s",
     "Reunions",
   ];
-  const [fiftiesDropdown, setFiftiesDropDown] = useState(false);
-  const [sixtiesDropdown, setSixtiesDropDown] = useState(false);
-  const [seventiesDropdown, setSeventiesDropDown] = useState(false);
-  const [eightiesDropdown, setEightiesDropDown] = useState(false);
-  const [nintiesDropdown, setNinetiesDropDown] = useState(false);
-  const [thousandsDropdown, setThousandsDropDown] = useState(false);
-  const [tensDropdown, setTensDropDown] = useState(false);
-  const yearDropdowns = [];
-  const initialState = {};
+
+  const yearsDropdown = {
+    yr1950s: false,
+    yr1960s: false,
+    yr1970s: false,
+    yr1980s: false,
+    yr1990s: false,
+    yr2000s: false,
+    yr2010s: false,
+    yr2020s: false,
+  };
+
+  const yearReducer = (state: any, action: any) => {
+    switch (action.type) {
+      case "CLICK_YEAR":
+        return {
+          ...state,
+          [action.variable]: !state[action.variable],
+        };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(yearReducer, yearsDropdown);
+  const handleToggle = (variable: any) => {
+    dispatch({ type: "CLICK_YEAR", variable });
+    console.log(state.yr1950s);
+  };
 
   return (
     <div>
@@ -32,29 +53,31 @@ function NavBar() {
       <div className="center-box side-margin">
         <div className="center-box">
           {YEARS.map((year) => {
-            return <p className="color-white small-right-margin">{year}</p>;
+            const cur_year = "yr" + year;
+            return (
+              <p
+                className="color-white small-right-margin"
+                onClick={() => handleToggle(cur_year)}
+              >
+                {year}
+              </p>
+            );
           })}
         </div>
       </div>
       {
-        <div className="absolute archive-dropdowns">
+        <div className="absolute archive-dropdowns dropdown-box">
           <div className="center-box side-margin">
             <div className="center-box archive-dropdowns">
-              {YEARS.map((year) => {
+              {YEARS.map((year: any, index: any) => {
+                const cur_year = "yr" + year;
                 return (
                   <div>
-                    {year !== "Reunions" && (
+                    {year !== "Reunions" && state[cur_year] && (
                       <ul className="color-white dropdown">
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
-                        <li>1950-1951</li>
+                        {increments[index].map((increment: any) => {
+                          return <li>{increment}</li>;
+                        })}
                       </ul>
                     )}
                   </div>
